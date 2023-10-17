b5042160b29ceb432f269ac6f2f5709397b6a1af
omar-t-ahmed <omartahmed@gmail.com>
2023-10-17 17:48:59 -0400

    view class

--- practice-for-ch-js-browser-ttt/src/ttt-view.js
+++ practice-for-ch-js-browser-ttt/src/ttt-view.js
@@ -6,21 +6,23 @@ class View {
   
   setupBoard(container) {
     let ele = document.createElement("ul");
-    ele.addEventListener("click", this.handleClick);
     container.append(ele);
     let ul = document.querySelector("ul")
-    for (let i = 0 ; i < 9; i++) {
-      let li = document.createElement("li")
-      li.innerText = "";
+    for (let i = 0 ; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        let li = document.createElement("li")
+        li.innerText = "";
+        li.setAttribute(pos, `${j}, ${y}`)
+      }
+      
       ul.append(li)
+      ele.addEventListener("click", this.handleClick);
     }
-    this.ele.addEventListener("click", function() {
-      this.handleClick(e)
-    });
   }
   
   handleClick(e) {
-    this.game.playMove(e)
+    let pos = e.target.pos
+    this.game.playMove(pos)
   }
 
   makeMove(square) {
